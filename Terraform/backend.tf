3c414d0e0b388beb84b8ac349734bdfb728b4320
Gowri Suresh <104151166+Gowri0109@users.noreply.github.com>
2026-04-10 13:11:51 +0530

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-file"   
+    bucket  = "terraform-state-file-gowri"   
     key     = "vpc/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
